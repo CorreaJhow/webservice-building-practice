48fc56b404e9c705645a07c4750860672b55337a
jhona <jhona@DESKTOP-7HQF0LP>
2023-06-12 21:22:51 -0300

    Add project files.

---
+++ List.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace WebService_Building
+{
+    public class List : List<string>
+    {
+    }
+}
---
+++ MyService.asmx.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Web.Services;
+
+namespace WebService_Building
+{
+
+    /// <summary>
+    /// Implementation of the MyService web service by merit of studies
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class MyService : System.Web.Services.WebService
+    {
+        private List<string> GreetingsArray = new List<string> { "Olá, mundo!", "Hello World!", "Hallo Welt!", "Hei Verden!", "pozdrowienia" };
+
+        //built-in database simulation
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public List<string> HelloWorldInSeveralLanguagens() 
+        {
+            var Greetings = GreetingsArray;
+
+            return Greetings;
+        }
+
+        [WebMethod]
+        public List<string> IncludeNewSaludation(string saludation)
+        {
+            GreetingsArray.Add(saludation);
+
+            return GreetingsArray;
+        }
+    }
+}
